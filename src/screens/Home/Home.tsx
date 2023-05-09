9517a6441e1da91539b4b65722e96814c80de764
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2023-05-09 12:21:31 +0900

    first structure

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles.css";
+
+export function Home() {
+  return (
+    <div className="container">
+      <div className="header">
+        <h1 className="title">My to-do list</h1>
+        <h2 className="subTitle">Lets do it!</h2>
+      </div>
+      <div className="form">
+        <input type="text" />
+        <button>+</button>
+      </div>
+      <p>list</p>
+    </div>
+  );
+}
