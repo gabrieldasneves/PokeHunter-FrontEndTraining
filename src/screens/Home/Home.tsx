e9e65d1cf66221cd2d432d0c144e998c21d8324d
uyasuda <yasudayutaro123@gmail.com>
2023-05-09 15:34:13 +0900

    fix

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -5,92 +5,92 @@ import { TodoItem } from "../../types/TodoItemsTypes";
 import { ToDo } from "../../components/ToDo/ToDo";
 
 export function Home() {
-  const [inputText, setInputText] = useState<string>("");
-  const [todoList, setTodoList] = useState<TodoItem[]>([]);
-  const [filterTypes, setFilterType] = useState<string>("all");
+    const [inputText, setInputText] = useState<string>("");
+    const [todoList, setTodoList] = useState<TodoItem[]>([]);
+    const [filterTypes, setFilterType] = useState<string>("all");
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputText(event.target.value);
-  };
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputText(event.target.value);
+    };
 
-  const handleAddButtonClick = () => {
-    if (inputText.trim() !== "") {
-      const newTodo: TodoItem = {
-        id: new Date().getTime(),
-        text: inputText,
-        isCompleted: false,
-      };
-      setTodoList([...todoList, newTodo]);
-      setInputText("");
-    } else {
-      alert("Please input your task name");
-    }
-  };
+    const handleAddButtonClick = () => {
+        if (inputText.trim() !== "") {
+            const newTodo: TodoItem = {
+                id: new Date().getTime(),
+                text: inputText,
+                isCompleted: false,
+            };
+            setTodoList([...todoList, newTodo]);
+            setInputText("");
+        } else {
+            alert("Please input your task name");
+        }
+    };
 
-  const handleCheckboxChange = (id: number) => {
-    const updateTodoList = todoList.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, isCompleted: !todo.isCompleted };
-      } else {
-        return todo;
-      }
-    });
-    setTodoList(updateTodoList);
-    console.log(todoList);
-  };
+    const handleCheckboxChange = (id: number) => {
+        const updateTodoList = todoList.map((todo) => {
+            if (todo.id === id) {
+                return { ...todo, isCompleted: !todo.isCompleted };
+            } else {
+                return todo;
+            }
+        });
+        setTodoList(updateTodoList);
+        console.log(todoList);
+    };
 
-  const handleFilter = (filter: string) => {
-    setFilterType(filter);
-  };
+    const handleFilter = (filter: string) => {
+        setFilterType(filter);
+    };
 
-  const filteredTodoList = () => {
-    if (filterTypes === "completed") {
-      return todoList.filter((todo) => todo.isCompleted);
-    } else if (filterTypes === "incomplete") {
-      return todoList.filter((todo) => !todo.isCompleted);
-    } else {
-      return todoList;
-    }
-  };
+    const filteredTodoList = () => {
+        if (filterTypes === "completed") {
+            return todoList.filter((todo) => todo.isCompleted);
+        } else if (filterTypes === "incomplete") {
+            return todoList.filter((todo) => !todo.isCompleted);
+        } else {
+            return todoList;
+        }
+    };
 
-  return (
-    <div className="container">
-      <div className="header">
-        <h1 className="title">My to-do list</h1>
-        <h2 className="subTitle">Lets do it!</h2>
-      </div>
-      <div className="form">
-        <input
-          id="taskInput"
-          placeholder="my to-do list"
-          type="text"
-          value={inputText}
-          onChange={handleInputChange}
-        />
-        <button onClick={handleAddButtonClick}>+</button>
-      </div>
+    return (
+        <div className="container">
+            <div className="header">
+                <h1 className="title">My to-do list</h1>
+                <h2 className="subTitle">Lets do it!</h2>
+            </div>
+            <div className="form">
+                <input
+                    id="taskInput"
+                    placeholder="my to-do list"
+                    type="text"
+                    value={inputText}
+                    onChange={handleInputChange}
+                />
+                <button onClick={handleAddButtonClick}>+</button>
+            </div>
 
-      <div style={{ display: "flex" }}>
-        <button onClick={() => handleFilter("all")}>Dispaly All</button>
-        <button onClick={() => handleFilter("inComplete")}>
-          Dispaly inComplete
-        </button>
-        <button onClick={() => handleFilter("completed")}>
-          Dispaly completed
-        </button>
-      </div>
-      <div className="todoListsContainer">
-        <ul>
-          {filteredTodoList().map((todo) => (
-            <ToDo
-              id={todo.id}
-              text={todo.text}
-              isCompleted={todo.isCompleted}
-              handleChange={() => handleCheckboxChange(todo.id)}
-            />
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+            <div style={{ display: "flex" }}>
+                <button onClick={() => handleFilter("all")}>Dispaly All</button>
+                <button onClick={() => handleFilter("incomplete")}>
+                    Dispaly inComplete
+                </button>
+                <button onClick={() => handleFilter("completed")}>
+                    Dispaly completed
+                </button>
+            </div>
+            <div className="todoListsContainer">
+                <ul>
+                    {filteredTodoList().map((todo) => (
+                        <ToDo
+                            id={todo.id}
+                            text={todo.text}
+                            isCompleted={todo.isCompleted}
+                            handleChange={() => handleCheckboxChange(todo.id)}
+                        />
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 }
