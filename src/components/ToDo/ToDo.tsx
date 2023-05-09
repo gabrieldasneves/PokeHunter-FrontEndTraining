e8eb1cfee3154d6056d2816e63fc02c331082cf6
uyasuda <yasudayutaro123@gmail.com>
2023-05-09 16:31:31 +0900

    add label

--- src/components/ToDo/ToDo.tsx
+++ src/components/ToDo/ToDo.tsx
@@ -3,32 +3,33 @@ import "./todo.css";
 import DELETE from "../../assets/delete.svg";
 
 type todo = TodoItem & {
-  handleChange: any;
+    handleChange: any;
 };
 
 export function ToDo({ id, isCompleted, handleChange, text }: todo) {
-  return (
-    <li key={id}>
-      <input
-        className=""
-        type="checkbox"
-        checked={isCompleted}
-        onChange={handleChange}
-      />
-      <span
-        style={{
-          textDecoration: isCompleted ? "line-through" : "none",
-          marginLeft: "5px",
-          marginRight: "5px",
-          maxWidth: "300px",
-          wordBreak: "break-all",
-        }}
-      >
-        {text}
-      </span>
-      <button id="delete">
-        <img src={DELETE} alt="delete" />
-      </button>
-    </li>
-  );
+    return (
+        <li key={id}>
+            <input
+                className=""
+                type="checkbox"
+                checked={isCompleted}
+                onChange={handleChange}
+                id={id.toString()}
+            />
+            <span
+                style={{
+                    textDecoration: isCompleted ? "line-through" : "none",
+                    marginLeft: "5px",
+                    marginRight: "5px",
+                    maxWidth: "300px",
+                    wordBreak: "break-all",
+                }}
+            >
+                <label htmlFor={id.toString()}>{text}</label>
+            </span>
+            <button id="delete">
+                <img src={DELETE} alt="delete" />
+            </button>
+        </li>
+    );
 }
